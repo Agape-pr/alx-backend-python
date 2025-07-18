2e9842130c33e374c94f2428272f96a8aae9d64c
apple <apple@agapes-Pro.local>
2025-07-18 14:31:21 +0800

    added all

---
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -0,0 +1,8 @@
+# utils.py
+
+def access_nested_map(nested_map, path):
+    """Access a nested map with a sequence of keys."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
+
