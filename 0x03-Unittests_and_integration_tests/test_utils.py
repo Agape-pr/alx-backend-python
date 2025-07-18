f123eb0f149eb94e69882caef9a471abf2af77b8
apple <apple@agapes-Pro.local>
2025-07-18 15:37:37 +0800

    added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -47,3 +47,28 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)   # Ensure get was called once with URL
             self.assertEqual(result, test_payload)       # Ensure returned value is expected
+
+
+# test_utils.py
+import unittest
+from unittest.mock import patch
+from utils import memoize
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -6,3 +6,20 @@ def access_nested_map(nested_map, path):
         nested_map = nested_map[key]
     return nested_map
 
+import requests
+
+def get_json(url):
+    return requests.get(url).json()
+
+# utils.py
+def memoize(method):
+    """Memoization decorator for methods"""
+    attr_name = "_{}".format(method.__name__)
+
+    @property
+    def wrapper(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, method(self))
+        return getattr(self, attr_name)
+
+    return wrapper
